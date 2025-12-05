4aa08d30de271ce7056350f0d53f20a0f379a30b
Arthur Woimbée <arthur.woimbee@gmail.com>
2025-12-05 18:04:20 +0100

    update libs, fix compilation, update readme

--- src/main.c
+++ src/main.c
@@ -50,7 +50,7 @@ void	init(t_data *dat, char *filename)
 	dat->fov = tan(1.22173047 / 2.);
 	if (!(dat->zbuff = malloc((dat->map.size.y + 1) * sizeof(t_vertices*)))
 		|| !(dat->zbuff[0] = malloc((dat->map.size.y + 1) * (dat->map.size.x + 1) * sizeof(t_vertices))))
-		msg_exit("cannot allocate enough memory.", 0);
+		ft_msg_exit("cannot allocate enough memory.", 0);
 	i = -1;
 	while (++i <= dat->map.size.y)
 		dat->zbuff[i] = &dat->zbuff[0][i * (dat->map.size.x + 1)];
@@ -58,7 +58,7 @@ void	init(t_data *dat, char *filename)
 
 void	usage(void)
 {
-	msg_exit("Usage : ./fdf <filename> [-p] [-c] [-z] [-res]\n\
+	ft_msg_exit("Usage : ./fdf <filename> [-p] [-c] [-z] [-res]\n\
 		\t-p precision: precision of rotation calculated as π/p, must be int\n\
 		\t-c basecolor: color in capitalized hex w/o \"0x\" (default: FF0000)\n\
 		\t-z zoom: dictate size of object, default to data->win_width / 2\n\
@@ -74,18 +74,18 @@ void	read_args(t_data *data, char **argv, int argc)
 	{
 		if (ft_strcmp(argv[i], "-p") == 0)
 			(data->mv = (float)ft_atoi(argv[++i])) == 0 ?
-			msg_exit("p cannot be equal to 0 !", 0) : 0;
+			ft_msg_exit("p cannot be equal to 0 !", 0) : 0;
 		else if (ft_strcmp(argv[i], "-c") == 0)
 			data->rgb = ft_atoi_base(argv[++i], "0123456789ABCDEF");
 		else if (ft_strcmp(argv[i], "-z") == 0)
 			(data->zoom = ft_atoi(argv[++i])) < 1 ?
-			msg_exit("minimum zoom is 1", 0) : 0;
+			ft_msg_exit("minimum zoom is 1", 0) : 0;
 		else if (ft_strcmp(argv[i], "-res") == 0 && i + 2 < argc)
 		{
 			(data->win_width = ft_atoi(argv[++i])) < 10 ?
-			msg_exit("Window width too small ! (%dpx)", &data->win_width) : 0;
+			ft_msg_exit("Window width too small ! (%dpx)", &data->win_width) : 0;
 			(data->win_height = ft_atoi(argv[++i])) < 10 ?
-			msg_exit("Window height too small ! (%dpx)", &data->win_height) : 0;
+			ft_msg_exit("Window height too small ! (%dpx)", &data->win_height) : 0;
 		}
 		else
 			usage();
@@ -103,7 +103,7 @@ int		main(int argc, char **argv)
 	t_data	*data;
 
 	if (!(data = ft_memalloc(sizeof(t_data))))
-		msg_exit("niet. malloc failed", 0);
+		ft_msg_exit("niet. malloc failed", 0);
 	if (argc == 1 || argv[1][0] == '-')
 		usage();
 	init(data, argv[1]);
@@ -111,8 +111,8 @@ int		main(int argc, char **argv)
 	chaos((data->mlx.win = mlx_new_window(data->mlx.ptr,
 		data->win_width, data->win_height, "Give good grade plz")));
 	render(&data->mlx, &data->map, data);
-	mlx_hook(data->mlx.win, 2, 1L, &keypress, data);
-	mlx_loop_hook(data->mlx.ptr, &exec_render, data);
+	mlx_hook(data->mlx.win, 2, 1L, (void*)&keypress, data);
+	mlx_loop_hook(data->mlx.ptr, (void*)&exec_render, data);
 	mlx_hook(data->mlx.ptr, 17, 0, (void*)exit, 0);
 	mlx_loop(data->mlx.ptr);
 	return (0);
--- src/read_map.c
+++ src/read_map.c
@@ -26,13 +26,13 @@ int				fill_map_line(char **data, t_map *map)
 	while (*tmp)
 		++tmp;
 	if (!(map->heightmap[map->size.y] = malloc((tmp - data + 1) * sizeof(int))))
-		msg_exit("cannot malloc heightmap line !", 0);
+		ft_msg_exit("cannot malloc heightmap line !", 0);
 	data_len = 0;
 	while (data[data_len])
 	{
 		height = ft_atoi(data[data_len]);
 		if (height == 0 && *data[data_len] != '0')
-			msg_exit("Invalid file ! read : %s", data[data_len]);
+			ft_msg_exit("Invalid file ! read : %s", data[data_len]);
 		map->heightmap[map->size.y][data_len] = height;
 		map->min = height < map->min ? height : map->min;
 		map->max = height > map->max ? height : map->max;
@@ -49,14 +49,14 @@ static size_t	map_height(char *filename)
 	char		*line;
 
 	if (!(fd = open(filename, O_RDONLY)))
-		msg_exit("Cannot open file \"%s\"", filename);
+		ft_msg_exit("Cannot open file \"%s\"", filename);
 	line_nb = 0;
 	while (get_next_line(fd, &line) > 0 && ++line_nb)
 		free(line);
 	if (line_nb == 0)
-		msg_exit("Empty file ?", 0);
+		ft_msg_exit("Empty file ?", 0);
 	if (close(fd) == -1)
-		msg_exit("Cannot close file", NULL);
+		ft_msg_exit("Cannot close file", NULL);
 	return (line_nb);
 }
 
@@ -91,7 +91,7 @@ t_map			*actually_read(t_map *map, int fd)
 		ft_free_tab(&tab);
 	}
 	if (i == -1)
-		msg_exit("Read error (malloc() or read() failed)", 0);
+		ft_msg_exit("Read error (malloc() or read() failed)", 0);
 	if (map->min == 0 && map->max == 0)
 		map->max = 1;
 	map->median = (map->min + map->max) / 2.;
@@ -107,10 +107,9 @@ t_map			*read_map(t_map *map, char *filename)
 	map->max = 0;
 	map->size.y = map_height(filename);
 	if (!(map->heightmap = malloc(map->size.y * sizeof(int*))))
-		msg_exit("Not enought memory.", 0);
+		ft_msg_exit("Not enought memory.", 0);
 	if (!(fd = open(filename, O_RDONLY)))
-		msg_exit("Cannot open file \"%s\"", filename);
+		ft_msg_exit("Cannot open file \"%s\"", filename);
 	map->size.y = 0;
 	return (actually_read(map, fd));
 }
-
